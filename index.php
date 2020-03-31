a4689070d877a38812d1eb92b3cb2f1c52817fe0
root <root@MalCaor>
2020-03-31 14:35:52 +0200

    is ok now

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
-<?php require_once "includes/functions.inc.php" ?>
-<script src="./includes/d3/d3.min.js"></script>
+<script type="module" src="./includes/d3/d3.min.js"></script>
 <script language="javascript" type="text/javascript"
 src="/javascript/jquery/jquery.js"></script>
 <div id="vumeterdiv" style="display: none;">
 <script type="text/javascript" src="<?php echo mtimePath('js/vumeterdiv.js')?>"></script>
 <div id="vumetergraph" style="height:516px;overflow: hidden;"></div>
 </div>
-<?php
+<?php 
+require_once "includes/functions.inc.php"
 echo '<script type="text/javascript">',
      'vumeterShow();',
      '</script>'
