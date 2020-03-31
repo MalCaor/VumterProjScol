657aa5065168f298cd344ca87563b11dc28ec7ba
root <root@MalCaor>
2020-03-31 13:50:26 +0200

    CA MARCHE PASx

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php require_once "includes/functions.inc.php" ?>
+<script src="./includes/d3/d3.min.js"></script>
 <script language="javascript" type="text/javascript"
 src="/javascript/jquery/jquery.js"></script>
-<script src="./includes/d3/d3.min.js"></script>
 <div id="vumeterdiv" style="display: none;">
 <script type="text/javascript" src="<?php echo mtimePath('js/vumeterdiv.js')?>"></script>
 <div id="vumetergraph" style="height:516px;overflow: hidden;"></div>
